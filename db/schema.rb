250e342d3c75b1fc4c1021780d5985f169e996cf
Jack Dempsey <jdempsey@undertone.com>
2014-01-10 15:09:16 -0800

    add in bootstrap views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110204705) do
+ActiveRecord::Schema.define(version: 20140110230641) do
 
   create_table "alerts", force: true do |t|
     t.string   "name"
